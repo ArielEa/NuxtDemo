df26e17c2db629eab003997f1dd6adfb393ebc95
Ariel <ariel@ArielnoMacBook-Air.local>
2024-10-10 11:05:12 +0900

    ~

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,6 +6,6 @@ export default defineNuxtConfig({
     'ant-design-vue/dist/reset.css',  // Ant Design Vue 的重置样式
   ],
   plugins: [
-    { src: '/Users/ariel/Public/CODES/Pages/NuxtjsDemo/nuxtdemo/plugins/ant-design-vue.js', ssr: false }
+    { src: '~/plugins/ant-design-vue.js', ssr: false }
   ]
 })
